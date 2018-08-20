43de3dff0c97a1f37b8908138c3c3317c978bed4
MCMrARM <mcmrarm@gmail.com>
2018-08-20 12:42:12 +0200

    Initial DCC server impl

---
+++ chatlib/src/main/java/io/mrarm/chatlib/irc/dcc/DCCIOHandler.java
@@ -0,0 +1,97 @@
+package io.mrarm.chatlib.irc.dcc;
+
+import java.io.IOException;
+import java.nio.channels.ClosedChannelException;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+
+public class DCCIOHandler extends Thread {
+
+    private Selector selector;
+
+    private static final DCCIOHandler instanceSingleton = new DCCIOHandler();
+    private boolean running = false;
+
+    public static DCCIOHandler getInstance() {
+        return instanceSingleton;
+    }
+
+    public DCCIOHandler() {
+        try {
+            selector = Selector.open();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    SelectionKey addServer(DCCServer server, ServerSocketChannel channel) throws ClosedChannelException {
+        SelectionKey ret = channel.register(selector, SelectionKey.OP_ACCEPT, server);
+        startIfNeeded();
+        selector.wakeup();
+        return ret;
+    }
+
+    SelectionKey addUploadSession(DCCServer.UploadSession session) throws ClosedChannelException {
+        SelectionKey ret = session.socket.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE, session);
+        startIfNeeded();
+        selector.wakeup();
+        return ret;
+    }
+
+    private void handleSelectionKey(SelectionKey k) throws IOException {
+        if (k.isAcceptable())
+            ((DCCServer) k.attachment()).doAccept();
+
+        if (k.attachment() instanceof DCCServer.UploadSession) {
+            if (k.isReadable())
+                ((DCCServer.UploadSession) k.attachment()).doRead();
+            if (k.isWritable())
+                ((DCCServer.UploadSession) k.attachment()).doWrite();
+            if (!k.isValid())
+                ((DCCServer.UploadSession) k.attachment()).close();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (this) {
+                if (selector.keys().size() <= 0) {
+                    running = false;
+                    break;
+                }
+            }
+            try {
+                selector.select();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            for (SelectionKey k : selector.selectedKeys()) {
+                try {
+                    handleSelectionKey(k);
+                } catch (IOException err) {
+                    err.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+
+    void startIfNeeded() {
+        synchronized (this) {
+            if (running)
+                return;
+            running = true;
+        }
+        while (isAlive()) { // wait until it stops if it's running
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException ignored) {
+            }
+        }
+        start();
+    }
+
+}
---
+++ chatlib/src/main/java/io/mrarm/chatlib/irc/dcc/DCCServer.java
@@ -0,0 +1,109 @@
+package io.mrarm.chatlib.irc.dcc;
+
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DCCServer implements Closeable {
+
+    private File file;
+    private ServerSocketChannel serverSocket;
+    private int socketLimit;
+    private List<UploadSession> sessions = new ArrayList<>();
+
+    public DCCServer(File file, int socketLimit) {
+        this.file = file;
+        this.socketLimit = socketLimit;
+    }
+
+    public int createServerSocket() throws IOException {
+        if (serverSocket == null) {
+            serverSocket = ServerSocketChannel.open();
+            serverSocket.socket().bind(new InetSocketAddress(0));
+            serverSocket.configureBlocking(false);
+            DCCIOHandler.getInstance().addServer(this, serverSocket);
+        }
+        return serverSocket.socket().getLocalPort();
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (serverSocket != null)
+            serverSocket.close();
+        for (UploadSession session : sessions)
+            session.close();
+    }
+
+    void doAccept() throws IOException {
+        if (socketLimit != -1 && sessions.size() >= socketLimit)
+            return;
+        SocketChannel socket = serverSocket.accept();
+        if (socket == null)
+            return;
+        new UploadSession(file, socket);
+    }
+
+
+    class UploadSession implements Closeable {
+
+        private ByteBuffer buffer = ByteBuffer.allocate(1024 * 1024);
+        private ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+        private FileChannel file;
+        private SelectionKey selectionKey;
+        SocketChannel socket;
+
+        UploadSession(File file, SocketChannel socket) throws IOException {
+            try {
+                this.file = new FileInputStream(file).getChannel();
+                this.socket = socket;
+                socket.configureBlocking(false);
+                selectionKey = DCCIOHandler.getInstance().addUploadSession(this);
+                sessions.add(this);
+            } catch (IOException e) {
+                close();
+                throw e;
+            }
+        }
+
+        @Override
+        public void close() {
+            sessions.remove(this);
+            if (selectionKey != null)
+                selectionKey.cancel();
+            try {
+                if (socket != null)
+                    socket.close();
+            } catch (IOException ignored) {
+            }
+            try {
+                if (file != null)
+                    file.close();
+            } catch (IOException ignored) {
+            }
+        }
+
+        void doRead() throws IOException {
+            while (socket.read(readBuffer) > 0); // ignore all the input
+        }
+
+        void doWrite() throws IOException {
+            int r;
+            while ((r = file.read(buffer)) > 0 || buffer.position() > 0) {
+                buffer.flip();
+                socket.write(buffer);
+                buffer.compact();
+            }
+        }
+
+    }
+
+}
