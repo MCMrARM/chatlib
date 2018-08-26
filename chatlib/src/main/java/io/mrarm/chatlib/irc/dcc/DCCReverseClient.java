0c349ccaef9fe0821e83f9487a3426e61815c3bb
MCMrARM <mcmrarm@gmail.com>
2018-08-26 15:15:18 +0200

    DCCReverseClient initial impl

---
+++ chatlib/src/main/java/io/mrarm/chatlib/irc/dcc/DCCReverseClient.java
@@ -0,0 +1,73 @@
+package io.mrarm.chatlib.irc.dcc;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+public class DCCReverseClient implements Closeable {
+
+    private FileChannel file;
+    private ServerSocketChannel serverSocket;
+    private DCCClient client;
+    private long offset;
+    private long size;
+
+    public DCCReverseClient(FileChannel file, long offset, long size) {
+        this.file = file;
+        this.offset = offset;
+        this.size = size;
+    }
+
+    public int createServerSocket() throws IOException {
+        if (serverSocket == null) {
+            serverSocket = ServerSocketChannel.open();
+            serverSocket.socket().bind(new InetSocketAddress(0));
+            serverSocket.configureBlocking(false);
+            DCCIOHandler.getInstance().register(serverSocket, SelectionKey.OP_ACCEPT, (SelectionKey k) -> {
+                if (k.isAcceptable())
+                    doAccept();
+            });
+        }
+        return serverSocket.socket().getLocalPort();
+    }
+
+    public int getPort() {
+        if (serverSocket == null)
+            return -1;
+        return serverSocket.socket().getLocalPort();
+    }
+
+    public DCCClient getClient() {
+        return client;
+    }
+
+    @Override
+    public void close() {
+        try {
+            if (serverSocket != null)
+                serverSocket.close();
+        } catch (IOException ignored) {
+        }
+        serverSocket = null;
+        if (client != null)
+            client.close();
+        client = null;
+    }
+
+    private void doAccept() throws IOException {
+        SocketChannel socket = serverSocket.accept();
+        if (socket == null)
+            return;
+        client = new DCCClient(socket, file, offset, size);
+        try {
+            serverSocket.close();
+        } catch (IOException ignored) {
+        }
+        serverSocket = null;
+    }
+
+}
